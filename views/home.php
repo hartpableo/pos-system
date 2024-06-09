5d00ffe3d288f3cee50c664bf504bab78c4eca16
Hart <hart.hpwebdev@gmail.com>
2024-06-09 19:10:39 +0800

    product card

--- views/home.php
+++ views/home.php
@@ -7,73 +7,19 @@
 
     <div class="container grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
 
-      <!-- Card -->
-      <div class="relative">
-        <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-          <img class="p-4 rounded-t-lg block" src="https://fastly.picsum.photos/id/906/300/200.jpg?hmac=WkkxahWfLBir2f3u6ewCC5eLdxOevIcK8dsEpaEN8W8" alt="product image" />
-          <div class="px-5 pb-5">
-            <a href="#">
-              <h5 class="text-xl font-semibold tracking-tight text-gray-900">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-            </a>
-            <div class="flex items-center mt-2.5 justify-between">
-              <span class="text-3xl font-bold text-gray-900">₱75</span>
-              <button type="button" class="btn">Add to cart</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- Card -->
+      <?php getTemplate('content/product-card'); ?>
+      <?php getTemplate('content/product-card'); ?>
+      <?php getTemplate('content/product-card'); ?>
+      <?php getTemplate('content/product-card'); ?>
+      <?php getTemplate('content/product-card'); ?>
+      <?php getTemplate('content/product-card'); ?>
+      <?php getTemplate('content/product-card'); ?>
+      <?php getTemplate('content/product-card'); ?>
+      <?php getTemplate('content/product-card'); ?>
+      <?php getTemplate('content/product-card'); ?>
+      <?php getTemplate('content/product-card'); ?>
+      <?php getTemplate('content/product-card'); ?>
 
-      <div class="relative">
-        <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-          <img class="p-4 rounded-t-lg block" src="https://fastly.picsum.photos/id/906/300/200.jpg?hmac=WkkxahWfLBir2f3u6ewCC5eLdxOevIcK8dsEpaEN8W8" alt="product image" />
-          <div class="px-5 pb-5">
-            <a href="#">
-              <h5 class="text-xl font-semibold tracking-tight text-gray-900">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-            </a>
-            <div class="flex items-center mt-2.5 justify-between">
-              <span class="text-3xl font-bold text-gray-900">₱75</span>
-              <button type="button" class="btn">Add to cart</button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="relative">
-        <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-          <img class="p-4 rounded-t-lg block" src="https://fastly.picsum.photos/id/906/300/200.jpg?hmac=WkkxahWfLBir2f3u6ewCC5eLdxOevIcK8dsEpaEN8W8" alt="product image" />
-          <div class="px-5 pb-5">
-            <a href="#">
-              <h5 class="text-xl font-semibold tracking-tight text-gray-900">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-            </a>
-            <div class="flex items-center mt-2.5 justify-between">
-              <span class="text-3xl font-bold text-gray-900">₱75</span>
-              <button type="button" class="btn bg-red-700">
-                Add to cart
-                <span>x1</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="relative">
-        <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-          <img class="p-4 rounded-t-lg block" src="https://fastly.picsum.photos/id/906/300/200.jpg?hmac=WkkxahWfLBir2f3u6ewCC5eLdxOevIcK8dsEpaEN8W8" alt="product image" />
-          <div class="px-5 pb-5">
-            <a href="#">
-              <h5 class="text-xl font-semibold tracking-tight text-gray-900">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-            </a>
-            <div class="flex items-center mt-2.5 justify-between">
-              <span class="text-3xl font-bold text-gray-900">₱75</span>
-              <button type="button" class="btn bg-red-700">
-                Add to cart
-                <span>x3</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
 
     </div>
 
