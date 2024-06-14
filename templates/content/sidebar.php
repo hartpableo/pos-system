7c8ffc3cdd0595b95a489287b8fc0d121d439d4c
Hart <hart.hpwebdev@gmail.com>
2024-06-14 11:08:22 +0800

    sidebar

--- templates/content/sidebar.php
+++ templates/content/sidebar.php
@@ -10,24 +10,26 @@
   </ul>
 
   <!-- Total -->
-<!--  <div class="flex justify-between items-center">-->
-<!--        <span class="block text-2xl font-bold">-->
-<!--          Total:-->
-<!--        </span>-->
-<!--    <span class="block text-3xl font-bold">-->
-<!--          ₱225-->
-<!--        </span>-->
-<!--  </div>-->
+  <div class="flex justify-between items-center">
+  <div class="block text-2xl font-bold">
+    Total:
+  </div>
+  <div class="block text-3xl font-bold">
+    ₱225
+  </div>
+  </div>
   <!-- Total -->
 
-<!--  <div class="flex justify-between items-stretch flex-wrap mt-10 gap-4">-->
-<!--    <button type="button" class="btn text-lg bg-gray-500 hover:bg-gray-400 focus:bg-gray-400 flex-grow">-->
-<!--      Cancel Order-->
-<!--    </button>-->
-<!--    <button type="button" class="btn text-lg bg-green-600 hover:bg-green-500 focus:bg-green-500 flex-grow">-->
-<!--      Proceed-->
-<!--    </button>-->
-<!--  </div>-->
+  <div class="flex justify-between items-stretch w-full mt-6">
+    <button type="button" class="cancel-order btn btn-error text-white">
+      Cancel Order
+    </button>
+    <form action="/checkout" method="POST">
+      <button type="submit" class="btn btn-success text-white">
+        Proceed
+      </button>
+    </form>
+  </div>
 
   <button type="button" class="sidebar-toggle">
     <span class="sr-only">Toggle Sidebar</span>
